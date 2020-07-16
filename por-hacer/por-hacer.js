499ad9893a77e8b3862d1f75abc18990661b83c0
soriro@hotmail.com <soriro@hotmail.com>
2020-07-16 13:23:23 -0500

    Primer commit

---
+++ por-hacer/por-hacer.js
@@ -0,0 +1,69 @@
+const fs = require('fs');
+
+const guardaDB = () => {
+    let data = JSON.stringify(listadoPorHacer);
+    fs.writeFile('db/data.json', data, (err) => {
+        if (err) throw new Error("No se puede grabar", err);
+    })
+
+}
+
+const cargarDB = () => {
+    try {
+        listadoPorHacer = require('../db/data.json');
+    } catch (error) {
+        listadoPorHacer = [];
+    }
+    //console.log(listadoPorHacer);
+}
+
+
+let listadoPorHacer = [];
+
+let getListado = () => {
+    cargarDB();
+    return listadoPorHacer;
+}
+
+const actualizar = (descripcion, completado = true) => {
+    cargarDB();
+    // let index = listadoPorHacer.findIndex(tarea => {
+    //     return tarea.descripcion === descripcion;
+    // })
+    let index = listadoPorHacer.findIndex(tarea => tarea.descripcion === descripcion);
+    if (index >= 0) {
+        listadoPorHacer[index].completado = completado;
+        guardaDB();
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const borrar = (descripcion) => {
+    cargarDB();
+    let index = listadoPorHacer.findIndex(tarea => tarea.descripcion == descripcion);
+    let borrado = listadoPorHacer.splice(index, 1);
+    guardaDB();
+    return borrado;
+}
+
+const crear = (descripcion) => {
+    cargarDB();
+
+    let porHacer = {
+        descripcion,
+        completado: false
+    };
+
+    listadoPorHacer.push(porHacer);
+    guardaDB();
+    return porHacer;
+}
+
+module.exports = {
+    crear,
+    getListado,
+    actualizar,
+    borrar
+}
